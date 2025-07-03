c12715a8604e9e446423bc710f69d6c7fa52ba42
astro1sumit <astro.sumit.usa@gmail.com>
2025-07-03 09:58:39 +0530

    Problem1_BuyTwoChocolates

--- Problem1_BuyTwoChocolates.java
+++ Problem1_BuyTwoChocolates.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Problem1_BuyTwoChocolates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int money = sc.nextInt();
+        int[] chocolates = new int[n];
+        for (int i = 0; i < n; i++) {
+            chocolates[i] = sc.nextInt();
+        }
+            Arrays.sort(chocolates);
+            int cost = chocolates[0] + chocolates[1];
+            if(cost<=money){
+                System.out.println(money - cost);
+            }else{
+                System.out.println(money);
+            }
+    }
 }
